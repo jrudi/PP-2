1636e861ef3a72d148ce81f6e97fe91af1a502ac
Jonas Rudi <jrudi@mail.uni-mannheim.de>
2016-04-02 21:17:27 +0200

    implemented Bomb, Building, Bullet, Explosion and Player

---
+++ src/objects/Bomb.java
@@ -0,0 +1,41 @@
+package objects;
+
+public class Bomb extends GameObject implements Damaging{
+
+	@Override
+	public int getCausingDamage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void generateExplosion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSilent(boolean silent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean collide(GameObject gameObjectToCheck) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void flipPolygonHorizontally() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean outOfView() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
